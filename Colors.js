fb25d9deea2a13671c99593933afc8ca37ee94aa
erfan <mahdi.farahmandsafa@gmail.com>
2025-05-16 14:13:47 +0330

    set text and view bg colors

---
+++ Colors.js
@@ -0,0 +1,10 @@
+export const Colors = {
+  dark: {
+    backgroundColor: "#27272a",
+    textColor: "#fafafa",
+  },
+  light: {
+    backgroundColor: "#fafafa",
+    textColor: "#27272a",
+  },
+};
