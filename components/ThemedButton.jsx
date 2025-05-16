e3ea08614cd86e89befc88b86d92a1e1da078f4f
erfan <mahdi.farahmandsafa@gmail.com>
2025-05-16 19:04:47 +0330

    create custom button

---
+++ components/ThemedButton.jsx
@@ -0,0 +1,47 @@
+import { StyleSheet, Pressable, useColorScheme } from "react-native";
+import ThemedText from "./ThemedText";
+import { Colors } from "../Colors";
+
+const ThemedButton = ({ title, city, setCity, style, ...props }) => {
+  const colorScheme = useColorScheme();
+  const theme = Colors[colorScheme] ?? Colors.dark;
+
+  return (
+    <Pressable
+      onPress={() => {
+        setCity(title);
+      }}
+      style={({ pressed }) => [
+        { backgroundColor: theme.backgroundColor },
+        styles.btn,
+        pressed && styles.pressed,
+        city === title && { backgroundColor: "#06b6d4" },
+        style,
+      ]}
+      {...props}
+    >
+      <ThemedText style={[styles.text]}>{title}</ThemedText>
+    </Pressable>
+  );
+};
+
+
+export default ThemedButton;
+
+const styles = StyleSheet.create({
+  btn: {
+    borderWidth: 2,
+    borderColor: "#a1a1aa",
+    borderRadius: 10,
+    padding: 10,
+    width: "30%",
+    alignItems: "center",
+  },
+  pressed: {
+    opacity: 0.5,
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "semibold",
+  },
+});
