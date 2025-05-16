8f02a6e765fa6a989e91de88f438bf5c7d12f98b
erfan <mahdi.farahmandsafa@gmail.com>
2025-05-16 14:15:19 +0330

    create base layout project

---
+++ app/_layout.jsx
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+import ThemedView from "../components/ThemedView";
+import Header from "../components/Header";
+import { Slot } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+
+const RootLayout = () => {
+  return (
+    <>
+      <StatusBar style="auto" />
+      <ThemedView style={styles.container}>
+        <Header />
+        <Slot />
+      </ThemedView>
+    </>
+  );
+};
+
+export default RootLayout;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    paddingTop: 40,
+    flex: 1,
+  },
+});
